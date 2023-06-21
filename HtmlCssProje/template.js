3176303129aebc7f391e14dba0ad3751b42cba1e
Aydan <aydanhilal5@gamil.com>
2023-06-21 23:24:25 +0400

    search, dark mode, header, footer

---
+++ HtmlCssProje/template.js
@@ -0,0 +1,21 @@
+// alert("Deneme")
+
+let darkMode = () => {
+    // alert("Dark mode")
+    window.document.classList.toggle("dark_mode")
+}
+
+// input search
+$(document).ready(function() {
+    const searchApi = ["Adana", "Balikesir", "Corum", "Denizli", "Van"];
+    $("#tags").autocomplete({
+        source: searchApi
+    })
+})
+
+
+// footer js
+let newDate = () => {
+    document.getElementById("date_id").innerHTML = new Date().getFullYear();
+}
+newDate()
