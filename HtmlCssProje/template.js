bfdaaee6b161eb15772786d7d42d77ee1ae7adc8
Aydan <aydanhilal5@gamil.com>
2023-06-24 18:57:27 +0400

    newspaperBlogContactScrollspyResponsivedesignLang

--- HtmlCssProje/template.js
+++ HtmlCssProje/template.js
@@ -2,7 +2,9 @@
 
 let darkMode = () => {
     // alert("Dark mode")
-    document.body.classList.toggle("dark_mode")
+    document.body.classList.toggle("dark_mode");
+    $(".card-text").css('color', '#0d6efd');
+    $(".card-title").css('color', 'black')
 }
 
 // input search
@@ -19,3 +21,15 @@ let newDate = () => {
     document.getElementById("date_id").innerHTML = new Date().getFullYear();
 }
 newDate()
+
+//scrollspy 
+$(document).ready(function () {
+    $('body').scrollspy({ target: '#listscroll' });
+})
+$(document).ready(function () {
+    $('body').on('activate.bs.scrollspy', function () {
+        var activeSection = $('#listscroll li.active a').attr('href');
+      
+        // Gerekirse ek eylemler gerçekleştirin
+      });
+})
